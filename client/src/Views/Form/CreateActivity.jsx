04db0c34425243dadd1b6833cd6ac5b9bd945e19
JosePenya <josemariapep9@gmail.com>
2023-08-25 18:40:37 -0500

    Sort: done; Filter: done; Form: done

--- client/src/Views/Form/CreateActivity.jsx
+++ client/src/Views/Form/CreateActivity.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { postActivities, getAllCountries, disassembleCountries } from '../../Redux/Actions/actions';
 import { Link } from 'react-router-dom';
@@ -37,6 +37,7 @@ const validation = (input) => {
 export default function CreateActivity() {
     const dispatch = useDispatch();
     const countries = useSelector(state => state.allCountries);
+    const [successMessage, setSuccessMessage] = useState("");
 
     useEffect(() => {
         dispatch(getAllCountries());
@@ -45,63 +46,50 @@ export default function CreateActivity() {
 
     //INICIO ========================================================================
     const [selectedCountries, setSelectedCountries] = useState([]);
+
     const countryOptions = countries.map(country => ({
-        key: country.id,
+        //key: country.id,
         id: country.id,
         value: country.name
     }));
+
     const handleCountrySelect = (selectedList) => {
+
         setSelectedCountries(selectedList);
+
+        const selectedCountryCodes = selectedList.map(country => country.id);
+        console.log(selectedCountryCodes)
+        setActivity({
+            ...activity,
+            countries: [...selectedCountryCodes]
+        })
     };
     //FIN ======================================================================
 
-    const [selectedCountryIds, setSelectedCountryIds] = useState([]);
     const [activity, setActivity] = useState({
         name: "",
         difficulty: "",
         duration: "",
-        season: ""
+        season: "",
+        countries: []
     });
+
     const [errors, setErrors] = useState({
         name: "",
         difficulty: "",
         duration: "",
         season: "",
-        countries: "",
+        countries: [],
     });
-
-    /*const validateField = (field, value) => {
-        return value.trim() === "" ? "Required field" : "";
-    };*/
-    /*const validateField = (field, value) => {
-        if (field === "name" || field === "duration") {
-            return value.trim() === "" ? "Required field" : "";
-        }
-        return "";
-    }*/
 
     const handleChange = (event) => {
         const { id, value } = event.target;
 
-        if (id === 'countryId') {
-            const selectedValue = value;
-            setSelectedCountryIds(prevIds =>
-                prevIds.includes(selectedValue)
-                    ? prevIds.filter(id => id !== selectedValue)
-                    : [...prevIds, selectedValue]
-            );
-        } else {
-            setActivity({
-                ...activity,
-                [id]: value
-            });
-
-            /*const errorMessage = validateField(id, value);
-            setErrors({
-                ...errors,
-                [id]: errorMessage
-            });*/
-        }
+        setActivity({
+            ...activity,
+            [id]: value
+        });
+        /*}*/
 
         setErrors(
             validation({
@@ -111,37 +99,47 @@ export default function CreateActivity() {
         )
     };
 
+    console.log("CAMPOSSSSS", activity.name)
+    console.log("CAMPO DURATION", activity.duration)
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        if (selectedCountryIds.length === 0) {
+        if (selectedCountries.length === 0) { // if (selectedCountryIds.length === 0) {
             alert("Select at least one country");
             return;
         }
 
-        const newActivity = {
+        //const selectedCountryCodes = selectedCountries.map(country => country.id);
+        /*const newActivity = {
+            
+        };*/
+        /*
+        console.log("AQUIIIIII", selectedCountryCodes)
+        setActivity({
             ...activity,
-            countries: selectedCountryIds
-        };
+            countries: selectedCountryCodes //selectedCountryIds
+        })
+        */
 
-        dispatch(postActivities(newActivity));
+        console.log("AQUIIIIII", activity)
+        dispatch(postActivities(activity));
 
         setActivity({
-            name: "",
+            name: '',
             difficulty: "",
-            duration: "",
-            season: ""
+            duration: '',
+            season: "",
+            countries: []
         });
 
-        setSelectedCountryIds([]);
-    };
+        setSelectedCountries([]);
 
-    /*const isSubmitDisabled = () => {
-        return selectedCountryIds.length === 0 || Object.values(activity).some(value => value.trim() === "");
+        setSuccessMessage("Activity created successfully!");
+        setTimeout(() => {
+            setSuccessMessage("");
+        }, 3000);
     };
-    const isSubmitDisabled = () => {
-        return selectedCountryIds.length === 0 || Object.values(errors).some(err => err.trim() !== "");
-    };*/
 
     return (
         <div>
@@ -150,10 +148,11 @@ export default function CreateActivity() {
                 <h3>Create new activity</h3>
             </div>
             <div>
+                {successMessage && <p className={style.successMessage}>{successMessage}</p>}
                 <form className={style.formContainer} onSubmit={handleSubmit}>
                     <div>
                         <label className={style.formLabel} htmlFor="name">Activity name: </label>
-                        <input onChange={handleChange} type="text" id='name' />
+                        <input onChange={handleChange} type="text" id='name' value={activity.name} />
                         {errors.name ? <label className={style.errorMessage}>{errors.name}</label> : null}
                     </div>
 
@@ -176,7 +175,7 @@ export default function CreateActivity() {
 
                     <div>
                         <label className={style.formLabel} htmlFor="duration">Duration in hours: </label>
-                        <input onChange={handleChange} type="text" id='duration' />
+                        <input onChange={handleChange} type="text" id='duration' value={activity.duration} />
                         {errors.duration ? <label className={style.errorMessage}>{errors.duration}</label> : null}
                     </div>
 
@@ -196,9 +195,7 @@ export default function CreateActivity() {
 
                     <br />
                     <div>
-                        <label className={style.formLabel} htmlFor="countryId">Countries: </label>
-
-                        {/* COMENTARIO */}
+                        <label className={style.formLabel} >Countries: </label>
                         <Multiselect
                             options={countryOptions}
                             displayValue="value"
@@ -207,9 +204,6 @@ export default function CreateActivity() {
                             onRemove={handleCountrySelect}
                             placeholder="Select countries"
                         />
-
-
-
                         {errors.countryId ? <label className={style.errorMessage}>{errors.countryId}</label> : null}
                     </div>
                     <br />
@@ -220,229 +214,3 @@ export default function CreateActivity() {
         </div >
     );
 }
-
-
-/*
-<select
-                            className={style.select}
-                            id="countryId"
-                            multiple
-                            value={selectedCountryIds}
-                            onChange={handleChange}
-                        >
-                            <option value="">Select countries</option>
-                            {countries.map(country => (
-                                <option key={country.id} value={country.id}>
-                                    {country.name}
-                                </option>
-                            ))}
-                        </select>
-*/
-
-
-
-
-/* import React, { useState, useEffect } from 'react'
-import { postActivities, getAllCountries, disassembleCountries } from '../../Redux/Actions/actions'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
-import style from './Form.module.css'
-
-export default function CreateActivity() {
-    const dispatch = useDispatch(); // Hook para despachar acciones de redux
-    const countries = useSelector(state => state.allCountries);
-
-    useEffect(() => {
-        dispatch(getAllCountries());
-        return () => dispatch(disassembleCountries())
-    }, [dispatch])
-
-    const [selectedCountryIds, setSelectedCountryIds] = useState([]);
-
-    const [activity, setActivity] = useState({ // Estado local: Almacena valores del formulario
-        name: "",
-        difficulty: "",
-        duration: "",
-        season: "",
-        countryId: ""
-    })
-    const [error, setError] = useState({ // Estado local: Almacena mensaje de error
-        name: "Required field",
-        difficulty: "Required field",
-        duration: "Required field",
-        season: "Required field",
-        countryId: "Required field"
-    })
-
-    //! Función que maneja el cambio en campo Activity
-    const handleChange = (event) => {
-        if (event.target.id === 'countryId') {
-            const selectedValue = event.target.value;
-            if (selectedCountryIds.includes(selectedValue)) {
-                setSelectedCountryIds(selectedCountryIds.filter(id => id !== selectedValue));
-            } else {
-                setSelectedCountryIds([...selectedCountryIds, selectedValue]);
-            }
-        } else {
-            setActivity({
-                ...activity,
-                [event.target.id]: event.target.value
-            });
-            validate({
-                [event.target.id]: event.target.value
-            }, event.target.id);
-        }
-    };
-
-    //! función: determina si el botón Enviar debe estar deshabilitado
-    const disable = () => {
-        return (
-            Object.values(error).some(err => err.trim() !== "Required field") ||
-            selectedCountryIds.length === ""
-        );
-    };
-
-    //! Validamos cada campo y actualiza los mensajes de error
-    const validate = (state, id) => {
-        if (id === 'name') {
-            if (state[id].trim() !== "") setError({
-                ...error,
-                name: ""
-            })
-            else setError({
-                ...error,
-                name: "Required field"
-            })
-        }
-        if (id === 'difficulty') {
-            if (state[id].trim() !== "") setError({
-                ...error,
-                difficulty: ""
-            })
-            else setError({
-                ...error,
-                difficulty: "Required field"
-            })
-        }
-        if (id === 'duration') {
-            if (state[id].trim() !== "") setError({
-                ...error,
-                duration: ""
-            })
-            else setError({
-                ...error,
-                duration: "Required field"
-            })
-        }
-        if (id === 'season') {
-            if (state[id].trim() !== "") setError({
-                ...error,
-                season: ""
-            })
-            else setError({
-                ...error,
-                season: "Required field"
-            })
-        }
-        if (id === 'countryId') {
-            if (state[id].length > 0) {
-                setError({
-                    ...error,
-                    countryId: ""
-                });
-            } else {
-                setError({
-                    ...error,
-                    countryId: "Select at least one country"
-                });
-            }
-        }
-    }
-
-    //! Para manejar el envío del formulario.
-    const handleSubmit = (event) => {
-        event.preventDefault() // Evita que se recargue la página.
-        dispatch(postActivities(activity)) // Envía la actividad creada al estado global.
-    }
-
-    return (
-        <div>
-            <div className={style.head}>
-                <h1>Tourist Activities</h1>
-                <h3>Create new activity</h3>
-            </div>
-            <div>
-                <form className={style.formContainer} onSubmit={handleSubmit}>
-
-                    <div>
-                        <label className={style.formLabel} htmlFor="name">Activity name: </label>
-                        <input onChange={handleChange} type="text" id='name' />
-                        <label className={style.errorMessage} htmlFor="name">{error.name}</label>
-                    </div>
-
-                    <br />
-
-                    <div>
-                        <label className={style.formLabel} htmlFor="difficulty">Difficulty: </label>
-                        <select onChange={handleChange} id='difficulty' value={activity.difficulty}>
-                            <option value="">Select difficulty</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                        <label className={style.errorMessage} htmlFor="difficulty">{error.difficulty}</label>
-                    </div>
-
-                    <br />
-
-                    <div>
-                        <label className={style.formLabel} htmlFor="duration">Duration in hours: </label>
-                        <input onChange={handleChange} type="text" id='duration' />
-                        <label className={style.errorMessage} htmlFor="duration">{error.duration}</label>
-                    </div>
-
-                    <br />
-
-                    <div>
-                        <label className={style.formLabel} htmlFor="season">Season: </label>
-                        <select onChange={handleChange} id='season' value={activity.season}>
-                            <option value={""}>Select season</option>
-                            <option value={"Spring"}>Spring</option>
-                            <option value={"Summer"}>Summer</option>
-                            <option value={"Autumn"}>Autumn</option>
-                            <option value={"Winter"}>Winter</option>
-                        </select>
-                        <label className={style.errorMessage} htmlFor="season">{error.season}</label>
-                    </div>
-
-                    <br />
-                    <div>
-                        <label className={style.formLabel} htmlFor="value1">Countries: </label>
-                        <select
-                            className={style.select}
-                            id="countryId"
-                            multiple
-                            value={selectedCountryIds}
-                            onChange={handleChange}
-                        >
-                            {countries.map(country => (
-                                <option key={country.id} value={country.id}>
-                                    {country.name}
-                                </option>
-                            ))}
-                        </select>
-
-                        <label className={style.errorMessage} htmlFor="countryId">{error.countryId}</label>
-                    </div>
-                    <br />
-                    <button disabled={disable()} type='Submit'>Create activity</button>
-                    <Link to='/home'>Cancel</Link>
-
-                </form>
-            </div >
-        </div >
-    )
-}
- */
